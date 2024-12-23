562eb63f2b2bad581eef056b84593dcfa5d2ee9a
rohan-compro <rohan.goel@comprotechnologies.com>
2024-12-23 15:47:09 +0530

    update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'My Site',
+  title: 'Rohan Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://rohan-compro.github.io',
   baseUrl: '/my-api-doc/',
